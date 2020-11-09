3eb0626d5976cccb195fd424af53ec8ce8500c57
tush-tr <iamtush-tr@DESKTOP-T3039R4.localdomain>
2020-11-09 18:07:53 +0530

    Data Types and Type Modifiers

---
+++ data_types_and_variables/type_modifier.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    short int si;
+    long int li;
+    cout <<"The size of short int: "<<sizeof(si)<<endl;
+    cout <<"The size of long int: "<<sizeof(li)<<endl;
+    return 0;
+}
