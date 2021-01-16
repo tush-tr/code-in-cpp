ffb4e32f7e07c81a29bc4f4d7132f21cff133af3
tush-tr <iamtush-tr@DESKTOP-T3039R4.localdomain>
2021-01-16 22:42:53 +0530

    Character arrays

--- arrays/char-arrays/que2.cpp
+++ arrays/char-arrays/que2.cpp
@@ -1,25 +1,40 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     // Largest word in a sentence
     int n;
-    cin>>n;
+    cin >> n;
     cin.ignore();
-    
-    char arr[n+1];
-    cin.getline(arr,n);
+
+    char arr[n + 1];
+    cin.getline(arr, n);
     cin.ignore();
 
     int mx = 0;
     int curr = 0;
-
-    for (int i = 0; i < n; i++)
+    int i = 0;
+    while (i<n)
     {
-        
-
-        /* code */
+        if (arr[i] == ' ' || arr[i] == '\0')
+        {
+            if (curr > mx)
+            {
+                mx = curr;
+            }
+            curr = 0;
+        }
+        else
+        {
+            curr++;
+        }
+        if (arr[i] == '\0')
+        {
+            break;
+        }
+        i++;
     }
-    
+    cout << mx << endl;
 
     return 0;
 }
