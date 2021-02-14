ad3edf7303912a05eba1f54e516590fa62c9d3d5
Tushar Rajpoot <tusharrajput604@gmail.com>
2021-02-14 09:26:50 +0000

    Reduce the given array using vector of pairs

--- dynamic-arrays/reduce-array.cpp
+++ dynamic-arrays/reduce-array.cpp
@@ -1,26 +1,32 @@
 #include<bits/stdc++.h>
+#include <climits>
+#include <utility>
 using namespace std;
-void display(vector<int>arr){
-    for(int i=0;i<arr.size();i++){
+void display(int arr[],int ln){
+    for(int i=0;i<ln;i++){
         cout<<arr[i]<<" ";
     }
 }
+bool myCompare(pair<int,int>p1,pair<int,int>p2){
+    return p1.first<p2.first;
+}
 int main()
 {
-    vector<int> n;
-    n = {10,16,7,14,5,3,12,9};
-    for(int j=0;j<n.size();j++)
-    {
-        int a;
-        for(int i=0;i<n.size();i++)
-        {
-            if(n[i]<n[i+1])
-            {
-            a = i;
-            }
+    int arr[] = {10,16,7,14,5,3,2,9};
+    vector<pair<int,int>> n;
+    int ln = sizeof(arr)/sizeof(arr[0]);
 
-        }
-        n[a] = j;
+    for(int i=0;i<ln;i++){
+        n.push_back(make_pair(arr[i],i));
     }
-    display(n);
+
+    
+    sort(n.begin(),n.end(),myCompare);
+
+    for(int i=0;i<n.size();i++){
+        arr[n[i].second] = i;
+    }
+
+
+    display(arr,ln);
 }
