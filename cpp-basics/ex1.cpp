14da57700d60845b5fcac56f4aee0c53a015eb35
tush-tr <tusharrajput604@gmail.com>
2023-12-21 23:04:39 +0530

    area of triangle program

---
+++ cpp-basics/ex1.cpp
@@ -0,0 +1,17 @@
+// Area of a Triangle
+// Given program is for finding area of a Triangle
+
+// Program is already having variable declare and initialised
+// Finding area of a Triangle as float using typecasting
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int b = 7;
+    int h = 5;
+    float area;
+    area = float(b*h)/2;
+    cout<<area<<endl;
+
+}
